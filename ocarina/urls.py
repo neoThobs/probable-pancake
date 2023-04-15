8920711c571f99fc2e224b41db0674ab6d904869
Oupa Bae <129596411+neoThobs@users.noreply.github.com>
2023-04-15 22:42:37 +0200

    feat: add projects views

--- ocarina/urls.py
+++ ocarina/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('hello/', include('hello_world.urls')),
+    path("projects/", include("projects.urls")),
 ]
